e252914422fa4f10407d37532f28a3e62fd2233a
Ruslan Kurbanov <kurbanovru@list.ru>
2022-08-31 02:14:08 +0300

    Урок 7. Созданы классы представлений

--- django_movie/django_movie/settings.py
+++ django_movie/django_movie/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -104,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
@@ -116,9 +117,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [STATIC_DIR]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- django_movie/movies/views.py
+++ django_movie/movies/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django.views.generic.base import View
+from flask import template_rendered
 
-# Create your views here.
+from .models import *
+
+
+class MoviesView(ListView):
+    """Список фильмов"""
+    model = Movie
+    queryset = Movie.objects.filter(draft=False)
+    template_name = 'movies/movie_list.html'
+    # def get(self, request):
+    #     movies = Movie.objects.all()
+    #     return render(request, 'movies/movies.html', {'movie_list': movies})
+
+
+class MovieDetailView(DetailView):
+    """Полное описание фильма"""
+    model = Movie
+    slug_field = 'url'
+    template_name = 'movies/movie_detail.html'
+    # def get(self, request, slug):
+    #     movie = Movie.objects.get(url=slug)
+    #     return render(request, 'movies/movie_detail.html', {'movie': movie})
+
