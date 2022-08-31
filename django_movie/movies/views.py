f45a535b6ff48a2080b06cd0bf17884019e363b6
Ruslan Kurbanov <kurbanovru@list.ru>
2022-08-31 19:21:42 +0300

    Yрок 9. Созданы отзывы и кадры из фильма

--- django_movie/movies/views.py
+++ django_movie/movies/views.py
@@ -1,7 +1,9 @@
+from django.shortcuts import redirect
 from django.views.generic import ListView, DetailView
 from django.views.generic.base import View
 from flask import template_rendered
 
+from .forms import *
 from .models import *
 
 
@@ -23,4 +25,15 @@ class MovieDetailView(DetailView):
     # def get(self, request, slug):
     #     movie = Movie.objects.get(url=slug)
     #     return render(request, 'movies/movie_detail.html', {'movie': movie})
+
+
+class AddReview(View):
+    def post(self, request, pk):
+        form = ReviewForm(request.POST)
+        movie = Movie.objects.get(id=pk)
+        if form.is_valid():
+            form = form.save(commit=False)
+            form.movie = movie
+            form.save()
+        return redirect(f'/review/{pk}')
 
