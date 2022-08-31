8f1f183dc4a4f11eb0e91cdfbf03c5a069949c40
Ruslan Kurbanov <kurbanovru@list.ru>
2022-08-31 20:33:08 +0300

    Yрок 10. добавлены ответы на отзывы

--- django_movie/movies/views.py
+++ django_movie/movies/views.py
@@ -33,6 +33,8 @@ class AddReview(View):
         movie = Movie.objects.get(id=pk)
         if form.is_valid():
             form = form.save(commit=False)
+            if request.POST.get("parent", None):
+                form.parent_id = int(request.POST.get("parent"))
             form.movie = movie
             form.save()
         return redirect(f'/review/{pk}')
