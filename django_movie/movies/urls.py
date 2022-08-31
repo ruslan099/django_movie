75a19e3ae15d100e244c70856150b0180a56047c
Ruslan Kurbanov <kurbanovru@list.ru>
2022-08-31 03:35:22 +0300

    Урок 8. Созданы шаблоны верстки с template блоками

--- django_movie/movies/urls.py
+++ django_movie/movies/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.MoviesView.as_view()),
+    path('', views.MoviesView.as_view(), name="index"),
     path('<slug:slug>/', views.MovieDetailView.as_view(), name='movie_detail'),
 ]
